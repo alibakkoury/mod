fdfd707ecb334954f8e3b7d07237430b3776c24b
Ubuntu <hamza@projet.2b0ekol0zeruplljoif2nzbouc.jx.internal.cloudapp.net>
2020-06-03 23:31:39 +0000

    train.py

--- train.py
+++ train.py
@@ -1,17 +1,18 @@
 import time
 import torch.utils.data
 from networks import ObjectDetection_SSD, LossFunction
+from data import DataLoader , Dataset
 import argparse
 import os 
 
 def get_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", default = "SSD300")
-    parser.add_argument("--batch_size", default = 32)
+    parser.add_argument("--batch_size", default = 1)
     parser.add_argument("--nb_epochs" , default = 200000)
     parser.add_argument("--lr" , default = 0.0001)
     parser.add_argument("--display_count" , default = 100)
-    parser.add_argument("--nbr_classes" , default = 500)
+    parser.add_argument("--nbr_classes" , default = 80)
     parser.add_argument("--checkpoint" , default = 50000)
     parser.add_argument("--checkpoint_dir" , default = 'checkpoints')
     parser.add_argument("--traindata_dir" , default = 'data/train/')
@@ -21,29 +22,36 @@ def get_opt():
 
 def train(model , opt , train_loader):
     
+    print("Training ...")
+    
     model.cuda()
     model.train()
 
     # criterion
-    box = model.create_boxes()
-    criterion = LossFunction(box)
+    box = model.create_boxes().cuda()
+    criterion = LossFunction(box).cuda()
     
     # optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
     
-    os.mkdir(opt.checkpoint_dir)
+    #os.mkdir(opt.checkpoint_dir)
     
     for epoch in range(opt.nb_epochs):
         iter_start_time = time.time()
         inputs = train_loader.next_batch()
         
-        img = inputs['img']
-        boxes = inputs['GT']
-        label = inputs['label']
+        img = inputs[0].cuda()
+        boxes = inputs[1]
+        labels = inputs[2]
+        
+
+        #for i in range(opt.batch_size):
+          #  boxes[i] = torch.cat(boxes[i] , dim=1)
+           # labels[i] = torch.cat(labels[i], dim=1)
 
         predicted_boxes , predicted_scores = model(img)
-
-        loss = criterion( predicted_boxes , predicted_scores , boxes , label )    
+        
+        loss = criterion(predicted_boxes , predicted_scores , boxes , labels )
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -60,11 +68,15 @@ def train(model , opt , train_loader):
 
 def main():
     opt = get_opt()
-    train_dataset = CPDataset(opt)
-    train_loader = CPDataLoader(opt, train_dataset)
+
+    
+    train_dataset = Dataset()
+    train_loader = DataLoader(opt, train_dataset)
 
     model = ObjectDetection_SSD(nbr_classes = opt.nbr_classes)
 
     train(model , opt , train_loader)
         
     print('Finished training')
+
+main()
